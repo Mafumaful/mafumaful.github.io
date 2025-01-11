f981eb44c750842d0937a5c4c48d71717d68a29e
mafumaful <2993821152@qq.com>
2025-01-10 20:24:59 -0500

    default

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/miakho/web/mafumaful.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/miakho/web/mafumaful.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/miakho/web/mafumaful.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/miakho/web/mafumaful.github.io/src/css/custom.css"),
+];
